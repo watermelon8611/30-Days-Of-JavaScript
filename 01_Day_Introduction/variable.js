d24edb6b7e76d5ec6691f6b7b3d20e41bef4b428
ConnorB <watermelon8611@gmail.com>
2024-03-02 14:14:50 -0600

    age change

--- 01_Day_Introduction/variable.js
+++ 01_Day_Introduction/variable.js
@@ -3,7 +3,7 @@ let firstName = 'Asabeneh' // first name of a person
 let lastName = 'Yetayeh' // last name of a person
 let country = 'Finland' // country
 let city = 'Helsinki' // capital city
-let age = 100 // age in years
+let age = 10 // age in years
 let isMarried = true
 
 // Declaring variables with number values
